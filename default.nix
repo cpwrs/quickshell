25042100012a5caaab1d02f779e7798d691d66d4
Carson Powers <me@carsonp.net>
2025-07-09 14:55:10 -0500

    Revert "feat(nix): nm buildInput"
    This reverts commit 8679f79d32fa86dcc0547c19c6c3c3f5966a14fc.

--- default.nix
+++ default.nix
@@ -20,7 +20,6 @@
   libdrm,
   libgbm ? null,
   pipewire,
-  networkmanager,
   pam,
 
   gitRev ? (let
@@ -41,7 +40,6 @@
   withWayland ? true,
   withX11 ? true,
   withPipewire ? true,
-  withNetworkManager ? true,
   withPam ? true,
   withHyprland ? true,
   withI3 ? true,
@@ -72,8 +70,7 @@
   ++ lib.optionals (withWayland && libgbm != null) [ libdrm libgbm ]
   ++ lib.optional withX11 xorg.libxcb
   ++ lib.optional withPam pam
-  ++ lib.optional withPipewire pipewire
-  ++ lib.optional withNetworkManager networkmanager;
+  ++ lib.optional withPipewire pipewire;
 
   cmakeBuildType = if debug then "Debug" else "RelWithDebInfo";
 
@@ -87,7 +84,6 @@
     (lib.cmakeBool "WAYLAND" withWayland)
     (lib.cmakeBool "SCREENCOPY" (libgbm != null))
     (lib.cmakeBool "SERVICE_PIPEWIRE" withPipewire)
-    (lib.cmakeBool "SERVICE_NETWORKMANAGER" withNetworkManager)
     (lib.cmakeBool "SERVICE_PAM" withPam)
     (lib.cmakeBool "HYPRLAND" withHyprland)
     (lib.cmakeBool "I3" withI3)
